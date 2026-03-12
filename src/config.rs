7b308cdce11d3bf7fb9c1b658ad7d6a9c8e88c33
HsuTing <hsuting0106@gmail.com>
2026-03-12 18:09:55 +0000

    perf(notion): use notion config, not env

--- src/config.rs
+++ src/config.rs
@@ -1,42 +1,34 @@
-use std::{env, str::FromStr};
+use std::{env, fs, path::PathBuf};
 
-use regex::{Error as RegexError, Regex};
+use serde::Deserialize;
 use serde_json::Value;
-use strum::ParseError as StrumParseError;
-use strum_macros::EnumString;
 use thiserror::Error;
 use uuid::Uuid;
 
 #[derive(Error, Debug)]
 pub enum ConfigError {
-    #[error("RegexError: {0}")]
-    Regex(#[from] RegexError),
-    #[error("ParseError: {0}")]
-    StrumParse(#[from] StrumParseError),
     #[error("NotFound")]
     NotFound,
 }
 
-#[derive(EnumString, Clone, PartialEq)]
+#[derive(Deserialize, Clone, PartialEq)]
 pub enum Permission {
     Get,
     Update,
     Add,
 }
 
-#[derive(EnumString, Default, Clone, PartialEq)]
-#[strum(serialize_all = "snake_case")]
+#[derive(Deserialize, Clone, PartialEq)]
 pub enum NotionType {
-    #[default]
     DataSource,
     Database,
 }
 
-#[derive(Default, Clone)]
+#[derive(Deserialize, Clone)]
 pub struct NotionConfig {
+    pub id: Option<String>,
     pub name: String,
     pub r#type: NotionType,
-    pub id: Option<String>,
     pub permission: Vec<Permission>,
 }
 
@@ -45,54 +37,15 @@ pub struct Config(Vec<NotionConfig>);
 
 impl Config {
     pub fn new() -> Result<Self, ConfigError> {
-        let mut notion_configs: Vec<NotionConfig> = Vec::new();
-        let notion_parttern = Regex::new(
-            r"^NOTION_(?<type>DATABASE|DATA_SOURCE)_(?<name>\w+)_(?<attr>ID|PERMISSION)$",
-        )?;
+        let home_dir = dirs::home_dir().unwrap_or("./".into());
+        let file_path = PathBuf::from(
+            env::var("NOTION_CONFIG")
+                .unwrap_or(home_dir.join(".config/notion.json").display().to_string()),
+        );
+        let config_str = fs::read_to_string(file_path).unwrap_or_default();
+        let config: Vec<NotionConfig> = serde_json::from_str(&config_str).unwrap_or_default();
 
-        for (key, value) in env::vars() {
-            if value.is_empty() {
-                continue;
-            }
-
-            if let Some(caps) = notion_parttern.captures(&key) {
-                let name = caps["name"].replace("_", "-").to_lowercase();
-                let r#type = caps["type"].replace("_", "-").to_lowercase();
-                let attr = caps["attr"].to_string();
-
-                if let Some(index) = notion_configs.iter().position(|c| c.name == name) {
-                    Config::update_notion_config_with_type(
-                        &mut notion_configs[index],
-                        attr,
-                        value,
-                    )?;
-                } else {
-                    let mut notion_config = NotionConfig {
-                        name,
-                        r#type: NotionType::from_str(&r#type)?,
-                        ..Default::default()
-                    };
-
-                    Config::update_notion_config_with_type(&mut notion_config, attr, value)?;
-                    notion_configs.push(notion_config);
-                }
-            };
-        }
-
-        Ok(Self(notion_configs))
-    }
-
-    fn update_notion_config_with_type(
-        notion_config: &mut NotionConfig,
-        attr: String,
-        value: String,
-    ) -> Result<(), ConfigError> {
-        match attr.as_ref() {
-            "ID" => notion_config.id = Some(value),
-            "PERMISSION" => notion_config.permission.push(Permission::from_str(&value)?),
-            _ => unreachable!("unknown type {}", attr),
-        }
-        Ok(())
+        Ok(Self(config))
     }
 
     pub fn get_id(
